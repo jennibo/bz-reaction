078df5c7da65b047b16f36a75c0acf7c0e364b2f
Viktorrindert <vi2511ri-s@student.lu.se>
2020-05-06 15:33:23 +0200

    Added trajectories to the phase portrait

--- PhasePortrait.py
+++ PhasePortrait.py
@@ -1,31 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def strogatz(x,y):
+# Returns the equations from the book by Strogatz
+def strogatz(Y):
     a = 1
     b = 1
-    return [a - x - 4 * x * y / (1+ x*x), b * x * (1 - y / (1 + x*x))]
-
-x = np.linspace(0, 1, 10)
-y = np.linspace(0, 1, 10)
+    x, y = Y
+    return np.array([a - x - 4 * x * y / (1+ x*x), b * x * (1 - y / (1 + x*x))])
+# Creates a meshgrid
+x = np.linspace(0, 1, 20)
+y = np.linspace(0, 1, 20)
 X, Y = np.meshgrid(x, y)
-
 ver = np.zeros(Y.shape)
 hor = np.zeros(Y.shape)
 NI, NJ = ver.shape
+# Calculates the gradients in every point of the meshgrid
 for i in range(NI):
     for j in range(NJ):
         x = X[i, j]
         y = Y[i, j]
-        uv = strogatz(x,y)
+        uv = strogatz([x, y])
         hor[i,j] = uv[0]
         ver[i,j] = uv[1]
 
-    
+# Plots the phase portrait    
 Q = plt.quiver(X, Y, hor, ver, color='r')
 
 plt.xlabel('$x$')
 plt.ylabel('$y$')
 plt.xlim([0, 1])
 plt.ylim([0, 1])
+
+# initial values [x0,y0] and t0
+y0 = np.array([0,0])
+#t = 0
+# Runge-Kutta method to solve paths
+h = 0.1 # step size
+steps = 100
+Y = np.zeros((steps, 2))
+Y[1, :] = y0
+for i in range(steps - 1):
+ #   t = t + h
+    k1 = strogatz(Y[i, :])
+    k2 = strogatz(Y[i, :] + h / 2 * k1)
+    k3 = strogatz(Y[i, :] + h / 2 * k2)
+    k4 = strogatz(Y[i, :] + h * k3)
+    yn = Y[i,:] + 1 / 6 * h * (k1 + 2 * k2 + 2 * k3 + k4)
+    Y[i + 1, :] = yn
+
+plt.plot(Y[:,0], Y[:,1], 'b-') # path
+plt.plot([Y[0,0]], [Y[0,1]], 'o') # start
+plt.plot([Y[-1,0]], [Y[-1,1]], 's') # end
+
+
 plt.savefig('phase-portrait.png')
