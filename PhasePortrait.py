2032c3f57a093fb28badf7b6511a919f65898a3e
Johan Boström <johan.bostrom@mollehem.se>
2020-05-18 08:55:05 +0200

    Formatting

--- PhasePortrait.py
+++ PhasePortrait.py
@@ -6,7 +6,9 @@ def strogatz(Y):
     a = 10
     b = 2
     x, y = Y
-    return np.array([a - x - 4 * x * y / (1+ x*x), b * x * (1 - y / (1 + x*x))])
+    return np.array([a - x - 4 * x * y / (1 + x * x), b * x * (1 - y / (1 + x * x))])
+
+
 # Creates a meshgrid
 xaxis = 5
 yaxis = 10
@@ -23,37 +25,36 @@ for i in range(NI):
         x = X[i, j]
         y = Y[i, j]
         uv = strogatz([x, y])
-        hor[i,j] = uv[0]
-        ver[i,j] = uv[1]
+        hor[i, j] = uv[0]
+        ver[i, j] = uv[1]
 
-# Plots the phase portrait    
-Q = plt.quiver(X, Y, hor, ver, color='r')
+# Plots the phase portrait
+Q = plt.quiver(X, Y, hor, ver, color="r")
 
-plt.xlabel('$x$')
-plt.ylabel('$y$')
+plt.xlabel("$x$")
+plt.ylabel("$y$")
 plt.xlim([0, xaxis])
 plt.ylim([0, yaxis])
 
 # initial values [x0,y0] and t0
-y0 = np.array([0,0])
-#t = 0
+y0 = np.array([0, 0])
+# t = 0
 # Runge-Kutta method to solve paths
-h = 0.01 # step size
+h = 0.01  # step size
 steps = 10000
 Y = np.zeros((steps, 2))
 Y[1, :] = y0
 for i in range(steps - 1):
- #   t = t + h
+    # t = t + h
     k1 = strogatz(Y[i, :])
     k2 = strogatz(Y[i, :] + h / 2 * k1)
     k3 = strogatz(Y[i, :] + h / 2 * k2)
     k4 = strogatz(Y[i, :] + h * k3)
-    yn = Y[i,:] + 1 / 6 * h * (k1 + 2 * k2 + 2 * k3 + k4)
+    yn = Y[i, :] + 1 / 6 * h * (k1 + 2 * k2 + 2 * k3 + k4)
     Y[i + 1, :] = yn
 
-plt.plot(Y[:,0], Y[:,1], 'b-') # path
-plt.plot([Y[0,0]], [Y[0,1]], 'o') # start
-plt.plot([Y[-1,0]], [Y[-1,1]], 's') # end
+plt.plot(Y[:, 0], Y[:, 1], "b-")  # path
+plt.plot([Y[0, 0]], [Y[0, 1]], "o")  # start
+plt.plot([Y[-1, 0]], [Y[-1, 1]], "s")  # end
 
-
-plt.savefig('phase-portrait.png')
+plt.savefig("phase-portrait.png")
--- gyorgyi_phase_portrait.py
+++ gyorgyi_phase_portrait.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from scipy.integrate import solve_ivp
 from gyorgyi_field import GyorgyiFieldModel
-kf = 3.23e-4 
+
+kf = 3.23e-4
 T = 0.5
 x0 = 0.05
 z0 = 9.0
@@ -12,19 +13,19 @@ tol = 1e-8
 
 gf = GyorgyiFieldModel(kf)
 Y0 = np.array([x0, z0, v0])
-l = 5 # Meshgrid = l*l 
+l = 5  # Meshgrid = l*l
 
 sol = solve_ivp(
     gf.rhs, (0, T), np.array([x0, z0, v0]), method="DOP853", atol=tol, rtol=tol
 )
 x = np.linspace(0.01, 0.56, l)
-z = np.linspace(np.power(10,0.3), np.power(10,1),l)
-v = np.linspace(0.845, 0.852,l)
+z = np.linspace(np.power(10, 0.3), np.power(10, 1), l)
+v = np.linspace(0.845, 0.852, l)
 
-colors = ['r','g','b','c','m','y','k']
+colors = ["r", "g", "b", "c", "m", "y", "k"]
 
 # Calculates and plots the X-Z phase portrait
-X, Y = np.meshgrid(x, z)        # "X and Y axis" in the phase portrait
+X, Y = np.meshgrid(x, z)  # "X and Y axis" in the phase portrait
 x_values = np.zeros(Y.shape)
 z_values = np.zeros(Y.shape)
 fig = plt.figure()
@@ -36,11 +37,11 @@ for h in range(l):
             ztemp = Y[i, j]
             xzv = np.array([xtemp, ztemp, vtemp])
             uv = gf.rhs(0, xzv)
-            x_values[i,j] = uv[0]
-            z_values[i,j] = uv[1]
-    plt.quiver (X, Y, x_values, z_values, color=colors[h])
-plt.plot(sol.y[0,:], sol.y[1,:])
-plt.show() 
+            x_values[i, j] = uv[0]
+            z_values[i, j] = uv[1]
+    plt.quiver(X, Y, x_values, z_values, color=colors[h])
+plt.plot(sol.y[0, :], sol.y[1, :])
+plt.show()
 
 # Calculates and plots the X-V phase portrait
 X, Y = np.meshgrid(x, v)
@@ -56,10 +57,10 @@ for h in range(l):
             vtemp = Y[i, j]
             xzv = np.array([xtemp, ztemp, vtemp])
             uv = gf.rhs(0, xzv)
-            x_values[i,j] = uv[0]
-            v_values[i,j] = uv[2]
-    plt.quiver (X, Y, x_values, v_values, color=colors[h])
-plt.plot(sol.y[0,:], sol.y[2,:])
+            x_values[i, j] = uv[0]
+            v_values[i, j] = uv[2]
+    plt.quiver(X, Y, x_values, v_values, color=colors[h])
+plt.plot(sol.y[0, :], sol.y[2, :])
 plt.show()
 
 # Calculates and plots the Z-V phase portrait
@@ -75,8 +76,8 @@ for h in range(l):
             vtemp = Y[i, j]
             xzv = np.array([xtemp, ztemp, vtemp])
             uv = gf.rhs(0, xzv)
-            z_values[i,j] = uv[1]
-            v_values[i,j] = uv[2]
-    plt.quiver (X, Y, z_values, v_values, color=colors[h])
-plt.plot(np.abs(sol.y[1,:]), np.abs(sol.y[2,:]))
+            z_values[i, j] = uv[1]
+            v_values[i, j] = uv[2]
+    plt.quiver(X, Y, z_values, v_values, color=colors[h])
+plt.plot(np.abs(sol.y[1, :]), np.abs(sol.y[2, :]))
 plt.show()
